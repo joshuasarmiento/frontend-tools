4f9eca32cd978bc3b6510090b3dc105f365deeae
Joshua Sarmiento <supremojoshua22@gmail.com>
2023-07-12 15:50:09 +0800

    update alert

--- src/views/AddData.vue
+++ src/views/AddData.vue
@@ -38,7 +38,7 @@
         <button type="submit" class="border border-neutral-200 px-6 py-1 hover:bg-neutral-200 hover:text-neutral-900">Submit</button>
     </form>
 
-    <section v-if="toggle" class="fixed top-10 right-5 animate-fade-left">
+    <section v-if="toggle" class="fixed right-0 bottom-0 animate-fade-left">
         <div role="alert" class="border border-gray-100 bg-white p-4 shadow-xl">
             <div class="flex items-start gap-4">
                 <span class="text-green-600">
@@ -55,13 +55,13 @@
                     </p>
                 </div>
 
-                <button @click="toggle = false" class="text-gray-500 transition hover:text-gray-600">
+                <!-- <button @click="toggle = false" class="text-gray-500 transition hover:text-gray-600">
                     <span class="sr-only">Dismiss popup</span>
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
-                </button>
+                </button> -->
             </div>
         </div>
     </section>
